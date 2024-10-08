7d91ccf6f6612a9ff8acb90f8ae6c624fecb38fe
{HITOMI} <{mhhh0931@gmail.com}>
2024-10-08 12:53:13 +0900

    add14

--- js/portfolio.js
+++ js/portfolio.js
@@ -7,4 +7,56 @@ new Splide(".splide",{
   speed: 1000, 
   focus  : "center",
   updateOnMove: true, 
+
+  classes: {
+    pagination: "splide__pagination your-class-pagination",
+    page: "splide__pagination__page your-class-page",
+  },
+  
 }).mount();
+
+
+// スムーススクロール
+//href属性の「#」で始まるリンクを全て取得
+const links = document.querySelectorAll('a[href^="#"]');
+//取得したリンクを1つずつ処理を実行する
+links.forEach((link) =>{
+　//リンクをクリックしたら処理を実行する
+  link.addEventListener('click', (e) => {
+　　//リンクイベントをキャンセルする
+    e.preventDefault();
+    //クリックしたリンクのhref属性を取得
+    const href = link.getAttribute('href');
+　　//目的のセクションを取得
+    const targetSection = document.querySelector(href);
+    //画面の上からセクションのtop位置までの垂直方向の距離
+    const sectionTop = targetSection.getBoundingClientRect().top;
+　　//現在位置を取得
+    const currentPos = window.scrollY;
+　　//ヘッダーの高さ
+    const gap = 80;
+    //現在位置から目的のsectionまでのスクロール量
+    const target = sectionTop + currentPos - gap;
+　　//特定の位置までスクロールさせる
+    window.scrollTo({
+      top: target,　　　　　//目的の位置のY座標を指定
+      behavior: 'smooth',　//スクロールの動きを指定
+    });
+  });
+});
+
+
+// スクロールフェードイン
+document.addEventListener('DOMContentLoaded', function() {
+  window.addEventListener('scroll', function() {
+      var elements = document.querySelectorAll('.service__item');
+      elements.forEach(function(element) {
+          var pos = element.offsetTop;
+          var scroll = window.scrollY;
+          var windowHeight = window.innerHeight;
+          if (scroll > pos - windowHeight) {
+              element.classList.add('scrolled');
+          }
+      });
+  });
+});
