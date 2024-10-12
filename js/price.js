82182e09aed66ad37c99ef3ba03b43017bca52fe
{HITOMI} <{mhhh0931@gmail.com}>
2024-10-12 13:56:58 +0900

    レスポンシブ修正

--- js/price.js
+++ js/price.js
@@ -21,9 +21,35 @@ document.addEventListener('DOMContentLoaded', function() {
   
     hamburger.addEventListener('click', function () {
         hamburger.classList.toggle("open");
-        nav.classList.toggle("add");
-    });
+        // nav.classList.toggle("add");
+
+        if (nav.style.display === 'none' || nav.style.display === '') {
+          // 厳密等価演算子（===）→値だけではなく、型まで含めて一致したときに「同じだよ～」な結果を返す
   
+          nav.style.display = 'block';
+          nav.style.opacity = 0; // 初期透明度を0に設定
+          var fadeEffect = setInterval(function() {
+              if (nav.style.opacity < 1) {
+                  nav.style.opacity = parseFloat(nav.style.opacity) + 0.1; // 透明度を徐々に上げる
+              } else {
+                  clearInterval(fadeEffect); // 完全に表示されたらインターバルをクリア
+              }
+          }, 1); // ミリ秒ごとに透明度を更新
+      } else {
+          var fadeOutEffect = setInterval(function() {
+              if (nav.style.opacity > 0) {
+                  nav.style.opacity = parseFloat(nav.style.opacity) - 0.1; // 透明度を徐々に下げる
+              } else {
+                  nav.style.display = 'none'; // 完全に非表示になったらdisplayをnoneに
+                  clearInterval(fadeOutEffect); // インターバルをクリア
+              }
+          }, 1); //ミリ秒ごとに透明度を更新
+      }
+  
+  });
+  
+  
+   
      // fadein
 
   window.addEventListener('scroll', function(){
@@ -43,3 +69,32 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
+
+  // スムーススクロール
+//href属性の「#」で始まるリンクを全て取得
+const links = document.querySelectorAll('a[href^="#"]');
+//取得したリンクを1つずつ処理を実行する
+links.forEach((link) =>{
+　//リンクをクリックしたら処理を実行する
+  link.addEventListener('click', (e) => {
+　　//リンクイベントをキャンセルする
+    e.preventDefault();
+    //クリックしたリンクのhref属性を取得
+    const href = link.getAttribute('href');
+　　//目的のセクションを取得
+    const targetSection = document.querySelector(href);
+    //画面の上からセクションのtop位置までの垂直方向の距離
+    const sectionTop = targetSection.getBoundingClientRect().top;
+　　//現在位置を取得
+    const currentPos = window.scrollY;
+　　//ヘッダーの高さ
+    const gap = 80;
+    //現在位置から目的のsectionまでのスクロール量
+    const target = sectionTop + currentPos - gap;
+　　//特定の位置までスクロールさせる
+    window.scrollTo({
+      top: target,　　　　　//目的の位置のY座標を指定
+      behavior: 'smooth',　//スクロールの動きを指定
+    });
+  });
+});
